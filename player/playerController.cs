90a1899f74b373a0b2ba0b54c73eded6e9a5ba8e
takaki <imagawa@takakinoMacBook-Pro.local>
2020-02-01 16:53:44 +0900

    first commit

---
+++ player/playerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    //Animator操作用
+    Animator animator;
+    public AudioClip sound;
+
+    //AudioSource操作用
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            this.animator.SetTrigger("Slash");
+            audioSource.PlayOneShot(sound);
+        }
+    }
+}
