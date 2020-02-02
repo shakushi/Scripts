5b700eb6f2a07ff53aaa6014c3bc937ca8b3f5bb
takaki <imagawa@takakinoMacBook-Pro.local>
2020-02-02 11:18:54 +0900

    Adjust timing and add goodhand

---
+++ Canvas/TimerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TimerController : MonoBehaviour
+{
+    //トータルの制限時間
+    private float totalTime;
+
+    //秒
+    [SerializeField]
+    public float seconds;
+    //前のupdate字の秒数 
+    private float oldSeconds;
+    //タイマー用テキスト
+    private Text timerText;
+
+    private bool isClear;
+    public GameObject obj;
+    private PanelAction act;
+
+    public static bool clearCheck = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalTime = seconds;
+        oldSeconds = 0f;
+        timerText = GetComponentInChildren<Text>();
+        isClear = false;
+        act = obj.GetComponent<PanelAction>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isClear)
+        {
+            //　制限時間が0秒以下なら何もしない
+            if (totalTime < 0f)
+            {
+                return;
+            }
+
+            //　トータルの制限時間を計算
+            totalTime -= Time.deltaTime;
+            //Debug.Log("totalTime = " + totalTime);
+
+            //　タイマー表示用UIテキストに時間を表示する
+            if (totalTime != oldSeconds)
+            {
+                // timerText.text = minute.ToString("0") + ":" + ((int)seconds).ToString("00");
+                timerText.text = (totalTime).ToString("00.00");
+            }
+            oldSeconds = totalTime;
+
+            if (totalTime <= 0f)
+            {
+                act.setFinish();
+                isClear = true;
+                SceneManager.LoadScene("result", LoadSceneMode.Additive);
+            }
+        }
+    }
+}
---
+++ goodhand.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;//uGUIにアクセス
+using System.Collections;
+
+public class goodhand : MonoBehaviour
+{
+    private Image image;
+    private float time;
+    public float fadetime;
+
+    //透明度操作用
+    SpriteRenderer spriteRenderer;
+    Color alpha = new Color(0, 0, 0, 1.0f);
+
+    private bool active = true;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        time = fadetime;//初期化
+        image = GetComponent<Image>();//Imageコンポネントを取得
+    }
+
+    void Update()
+    {
+        time -= Time.deltaTime;//時間更新(徐々に減らす)
+        float f = time / fadetime; //徐々に0に近づける
+        float a = 1 - time / fadetime;//徐々に1に近づける
+        if (a > 0.9f && active)
+        {
+            //Debug.Log("destroy");
+            Destroy(this.gameObject, 0.2f);
+            active = false;
+        }
+        else if (active)
+        {
+            Vector2 trans = this.transform.position;
+            trans.y += f * 0.08f;
+            this.transform.position = trans;
+
+            this.spriteRenderer.material.color = alpha * a;
+        }
+    }
+}
